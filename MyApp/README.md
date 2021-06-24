d1407659e9d891f2bb695ede4e2c59fdb62cb23e
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-06-24 15:46:26 +0000

    Changing to GraphQL and edits

--- MyApp/README.md
+++ MyApp/README.md
@@ -1,3 +1,4 @@
 # GitAPI Module
 > A module made by @JBloves27
 
+Made with [python3]()
--- MyApp/git_api/main.py
+++ MyApp/git_api/main.py
@@ -1,43 +1,96 @@
 import json
 import requests
-from bs4 import BeautifulSoup
+import os
+#from bs4 import BeautifulSoup
 
 #res = requests.get("https://api.github.com/users/"+self.user)#.json()
+
+
+
+url = "https://api.github.com/graphql"
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"bearer {os.environ['token']}"
+}
+
+
+USER = """
+bio
+name
+email
+avatarUrl
+createdAt
+isSiteAdmin
+location
+twitterUsername
+isDeveloperProgramMember"""
+
+followerQuery = """
+query defualtUserData($username: String!, $followerCount: Int!, $followingCount: Int!) {
+  user(login: $username) {
+  	followers(first: $followerCount) {
+      users: nodes {
+        name: login
+      }
+    }
+    following(first: $followingCount) {
+      users: nodes {
+        name:login
+      }
+    }
+  }
+}"""
+
 
 class InvalidStr(Exception):
   pass
 class InvalidUser(Exception):
   pass
 
-class user:
-  def __init__(self, username: str):
-    if type(username) != str:
-      raise InvalidStr("The 'username' arguement is not a string!")
-    self.user = username
-    res = requests.get(f"https://github.com/{self.user}", headers={"User-Agent": "Mozilla/2.0"})
-    if res.status_code == 404:
-      raise InvalidUser("No such user exists!")
-    self.res = res
-    soup = BeautifulSoup(res.content, 'html.parser')
-    self.soup = soup
 
-  def name(self):
+class user:
+  def __init__(self, username):
+    body = {'query':'query defualtUserData($username: String!) {user(login: "'+username+'") { '+USER+' } }'}
+    self.user = username
+    self.res = json.loads(requests.post(url, data=body, headers=headers).text)#['data']['user']
+    if self.res is None:
+      raise InvalidUser("No such GitHub user exists!")
+    self.bio = self.res["bio"]
+    self.name = self.res["name"]
+    self.email = self.res["email"]
+    self.avatar = self.res["avatarUrl"]
     
 
-    self.name = None
-    return self.name
+  def name(self):
+    body = {
+          'query': """query user($username: String!) {
+                      user: user(login: $username) {
+                        name() {
+                          items {
+                            """+USER+"""
+                          }
+                        }
+                      }
+                    }
+            """,
+            "variables": json.dumps({
+              "username": self.res["username"],
+            })
+          }
+    return json.dumps(json.loads(requests.post(url, data=body,headers=headers).text), sort_keys=True, indent=2)
+    #return self.name
 
   def followers(self):
 
 
-    self.followers = None #to be named
     return self.followers
 
   def status(self):
-    for i in self.soup.find_all('div', attrs={'class':'js-user-status-container user-status-circle-badge d-inline-block lh-condensed-ultra p-2'}):
-      status = self.soup.find("div", attrs={'class':"css-truncate css-truncate-target width-fit color-text-primary"})
-      for x in i:
-        if x == status:
-          self.status = status
-          return self.status
 
+    return self.status
+
+
+#user("JBYT27").name()
+body = {'query':'query defualtUserData($username: String!) {user(login: "JBYT27") { '+USER+' } }'}
+json.loads(requests.post(url, data=body, headers=headers).text)['data']['user']
