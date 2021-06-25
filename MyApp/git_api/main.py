88c23a8a7344688a0709b2213f600c7aea8db6c8
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-06-25 15:57:07 +0000

    Redesign-ed main.py and added Token, User, and Name function

--- MyApp/git_api/main.py
+++ MyApp/git_api/main.py
@@ -1,96 +1,81 @@
-import json
 import requests
 import os
-#from bs4 import BeautifulSoup
-
-#res = requests.get("https://api.github.com/users/"+self.user)#.json()
+import json
 
 
-
-url = "https://api.github.com/graphql"
-
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"bearer {os.environ['token']}"
-}
-
-
+url = 'https://api.github.com/graphql'
 USER = """
-bio
-name
-email
-avatarUrl
-createdAt
-isSiteAdmin
-location
-twitterUsername
-isDeveloperProgramMember"""
-
-followerQuery = """
-query defualtUserData($username: String!, $followerCount: Int!, $followingCount: Int!) {
-  user(login: $username) {
-  	followers(first: $followerCount) {
-      users: nodes {
-        name: login
-      }
-    }
-    following(first: $followingCount) {
-      users: nodes {
-        name:login
-      }
-    }
-  }
-}"""
+  bio
+  email
+  avatarLink:avatarUrl
+  accountCreatedAt:createdAt
+  isAdmin:isSiteAdmin
+  location
+  name
+  twitterUsername
+  isDevMember:isDeveloperProgramMember
+  userId:databaseId
+  pinnedItems:anyPinnableItems 
+"""
 
 
-class InvalidStr(Exception):
-  pass
-class InvalidUser(Exception):
-  pass
+class asdf:
+  def run_query(query, token):
+        try:
+          headers = {"Authorization": "Bearer " + token}
+        except:
+          raise Exception("Token has not been named by token function")
+        request = requests.post(url, json={'query': query}, headers=headers)
+        if request.status_code == 200:
+          return json.dumps(request.json(), indent=2, sort_keys=True)
+        else:
+          raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
+def Token(TOKEN=None):
+  global token
+  if TOKEN == None:
+    raise Exception("TOKEN argument must be filled out!")
+  else:
+    token = TOKEN
 
 class user:
   def __init__(self, username):
-    body = {'query':'query defualtUserData($username: String!) {user(login: "'+username+'") { '+USER+' } }'}
+    try:
+      self.token = token
+    except:
+      raise Exception("Token has not been named by token function")
     self.user = username
-    self.res = json.loads(requests.post(url, data=body, headers=headers).text)#['data']['user']
-    if self.res is None:
-      raise InvalidUser("No such GitHub user exists!")
-    self.bio = self.res["bio"]
-    self.name = self.res["name"]
-    self.email = self.res["email"]
-    self.avatar = self.res["avatarUrl"]
+    query = """
+      query UserData { 
+          user(login: \"""" + self.user + """\") { 
+            """+USER+"""
+          } 
+      }
+    """
+    self.query = query
     
+    #if self.token == None:
+    #  raise Exception("Token must be inputted!")
+    if self.user == None:
+      raise Exception("Username must be inputted!")
+    
+    #return asdf.run_query(query, self.token)
 
-  def name(self):
-    body = {
-          'query': """query user($username: String!) {
-                      user: user(login: $username) {
-                        name() {
-                          items {
-                            """+USER+"""
-                          }
-                        }
-                      }
-                    }
-            """,
-            "variables": json.dumps({
-              "username": self.res["username"],
-            })
-          }
-    return json.dumps(json.loads(requests.post(url, data=body,headers=headers).text), sort_keys=True, indent=2)
-    #return self.name
-
-  def followers(self):
+  def User(self):
+    self.userf = asdf.run_query(self.query, self.token)
+    return self.userf
 
 
-    return self.followers
+  def Name(self):
+    query = """
+      query UserData { 
+          user(login: \"""" + self.user + """\") { 
+            name
+          } 
+      }
+    """
+    self.name = asdf.run_query(query, self.token)
+    return self.name
 
-  def status(self):
-
-    return self.status
-
-
-#user("JBYT27").name()
-body = {'query':'query defualtUserData($username: String!) {user(login: "JBYT27") { '+USER+' } }'}
-json.loads(requests.post(url, data=body, headers=headers).text)['data']['user']
+# Token(os.environ["token"])
+# print(user("JBYT27").name())
