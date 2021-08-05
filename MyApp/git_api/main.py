acf1bb5385a7e0db2e5f474be2bf39ee9dba7207
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-08-05 21:44:12 +0000

    Changes

--- MyApp/git_api/main.py
+++ MyApp/git_api/main.py
@@ -18,32 +18,37 @@ USER = """
   pinnedItems:anyPinnableItems 
 """
 
+class ArgumentError(Exception):
+  pass
+class QueryFailError(Exception):
+  pass
+
 
 class asdf:
   def run_query(query, token):
         try:
           headers = {"Authorization": "Bearer " + token}
         except:
-          raise Exception("Token has not been named by token function")
+          raise ArgumentError("Token has not been named by token function!")
         request = requests.post(url, json={'query': query}, headers=headers)
         if request.status_code == 200:
           return json.dumps(request.json(), indent=2, sort_keys=True)
         else:
-          raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+          raise QueryFailError("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 def Token(TOKEN=None):
   global token
   if TOKEN == None:
-    raise Exception("TOKEN argument must be filled out!")
+    raise ArgumentError("TOKEN argument must be filled out!")
   else:
     token = TOKEN
 
-class user:
+class User:
   def __init__(self, username):
     try:
       self.token = token
     except:
-      raise Exception("Token has not been named by token function")
+      raise ArgumentError("Token has not been named by token function!")
     self.user = username
     query = """
       query UserData { 
@@ -77,5 +82,5 @@ class user:
     self.name = asdf.run_query(query, self.token)
     return self.name
 
-# Token(os.environ["token"])
-# print(user("JBYT27").name())
+Token(os.environ["token"])
+print(User("JBYT27").User())
---
+++ README.md
@@ -0,0 +1,4 @@
+# GitAPI: An API made for GitHub Stats!
+> Coded in Python3, uploaded to PyPi, and coded by JBYT27
+
+## Introduction
